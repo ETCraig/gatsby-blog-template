c8598eae74a8cbc43d606fd3e3032fd7bf6b8ee1
Craig <dev.etcraig@gmail.com>
2023-01-01 20:35:08 -0700

    Styled Components Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,45 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const IndexPage = ({ data}) => (
+const Title = styled.h1`
+  display: inline-block;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  &:hover {
+    color: #1dcaff;
+  }
+`
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <div>
-      <h1>Thoughts by Craig</h1>
+      <Title>Thoughts by Craig</Title>
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <span to={node.fields.slug}>
-            <h4>
+        <BlogBody key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
               {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
-            </h4>
-          </span>
+            </BlogTitle>
+          </BlogLink>
           <p>{node.frontmatter.description || node.excerpt}</p>
-        </div>
+        </BlogBody>
       ))}
     </div>
   </Layout>
